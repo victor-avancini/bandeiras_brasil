ca0f27b187f97c4f4a730c5a681673dca43dd30d
Victor Avancini <victor.avancini@hotmail.com>
2024-04-04 22:49:30 -0300

    Componente footer criado

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
+import styles from "./style.module.scss";
+
 export const Footer = () => {
     return (
         <footer>
-            
+            <div className={styles.created}>
+                <p>Criado por <a href="https://victoravancini.vercel.app/" target="_blank">Victor Avancini</a></p>
+            </div>
+            <div className={styles.message}>
+                <p>Gostou desse projeto? Me mande uma mensagem</p>
+            </div>
         </footer>
     )
 }
