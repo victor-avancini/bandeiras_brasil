bb216a33d292201f61c7df4fbecdeb4bcc22107f
Victor Avancini <victor.avancini@hotmail.com>
2024-05-15 20:14:38 -0300

    Implemented the ColorSection on the States Pages

--- src/pages/StatePage/StatePage.tsx
+++ src/pages/StatePage/StatePage.tsx
@@ -8,6 +8,7 @@ import { SourcesSection } from "../../sections/SourcesSection/SourcesSection";
 import { Helmet } from "react-helmet-async";
 import { Pagination } from "../../components/Pagination/Pagination";
 import { CompositionSection } from "../../sections/CompositionSection/CompositionSection";
+import { ColorsSection } from "../../sections/ColorsSection/ColorsSection";
 
 export const StatePage: React.FC<StatePageProps> = ({ state }) => {
     return (
@@ -20,6 +21,7 @@ export const StatePage: React.FC<StatePageProps> = ({ state }) => {
             <HeaderState state={state} />
             <DescriptionSection state={state} />
             <CompositionSection state={state}/>
+            <ColorsSection state={state}></ColorsSection>
             <Pagination state={state} />
             <SourcesSection state={state} />
             <Footer />
