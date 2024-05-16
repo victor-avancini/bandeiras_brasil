43c51ce4619854b0a7dfb48d062c20caca209513
Victor Avancini <victor.avancini@hotmail.com>
2024-05-15 22:40:10 -0300

    Implemented ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { HelmetProvider } from "react-helmet-async";
 import { RoutesMain } from "./routes/RoutesMain";
-import "./styles/styles.css"
+import "./styles/styles.css";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
 
   return (
     <HelmetProvider>
+      <ScrollToTop/>
       <RoutesMain />
     </HelmetProvider>
   )
